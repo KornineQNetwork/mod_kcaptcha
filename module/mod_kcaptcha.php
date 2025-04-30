3cc86ba425401f4dbaeb8ba674dcb18e19f79250
KornineQ <84327578+KornineQ@users.noreply.github.com>
2025-04-30 22:33:48 +0200

    1.2

--- module/mod_kcaptcha.php
+++ module/mod_kcaptcha.php
@@ -1,55 +1,56 @@
 <?php
 class mod_kcaptcha extends ModuleHelper {
-	private $kcaptcha;
-	private $kaptcha_api = "https://sys.kornineq.de/kaptcha/kaptcha-verify.php";
-	private $sitekey = "";
-	private $sitesecret = "";
+        private $kcaptcha;
+        private $kaptcha_api = "https://sys.kornineq.de/kaptcha/kaptcha-verify.php";
+        private $sitekey = "";
+        private $sitesecret = "";
 
-	public function getModuleName(){
-		return 'mod_kcaptcha : mod_kcaptcha';
-	}
+        public function getModuleName(){
+                return 'mod_kcaptcha : mod_kcaptcha';
+        }
 
-	public function getModuleVersionInfo(){
-		return '1.1';
-	}
+        public function getModuleVersionInfo(){
+                return '1.2';
+        }
 
-	public function autoHookHead(&$head, $isReply){
-		//nothing
-	}
+        public function autoHookHead(&$head, $isReply){
+                //nothing
+        }
 
-	public function autoHookPostForm(&$txt){
+        public function autoHookPostForm(&$txt){
         $txt .= '<tr><th class="postblock">Verification</th><td>'.'<script src="https://sys.kornineq.de/kaptcha/kaptcha.js?536346s" data-sitekey="'.$this->sitekey.'" type="text/javascript"></script>'.'</td></tr>';
-	}
-	
-	function kaptcha_validate() {
-	    if(!isset($_POST["_KAPTCHA"])) { return false; }
-	   
-	    $postFields = [
-	        'key' => $_POST["_KAPTCHA_KEY"],
-	        'site_key' => $this->sitekey,
-	        'site_secret' => $this->sitesecret
-	    ];
-   
-	    $ch = curl_init($this->kaptcha_api);
-	    curl_setopt($ch, CURLOPT_POST, 1);
-	    curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    
-	    $result = curl_exec($ch);
-	    curl_close($ch);
-    
-	    $response = json_decode($result, true);
-   
-	    return isset($response['success']) && $response['success'] === true;
-	}
-	
-	function check_captcha() {
-		if (isset($_POST["_KAPTCHA"]))
-			return $this->kaptcha_validate();
-		return false;
-	}
+        }
 
-	public function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo){
-		if(!$this->check_captcha()){ die('You are not acting like a human!'); } 
-	}
+        function kaptcha_validate() {
+            if(!isset($_POST["_KAPTCHA"])) { return false; }
+
+            $postFields = [
+                'key' => $_POST["_KAPTCHA_KEY"],
+                'kaptcha_value' => $_POST["_KAPTCHA"],
+                'site_key' => $this->sitekey,
+                'site_secret' => $this->sitesecret
+            ];
+
+            $ch = curl_init($this->kaptcha_api);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            $response = json_decode($result, true);
+
+            return isset($response['success']) && $response['success'] === true;
+        }
+
+        function check_captcha() {
+                if (isset($_POST["_KAPTCHA"]))
+                        return $this->kaptcha_validate();
+                return false;
+        }
+
+        public function autoHookRegistBegin(&$name, &$email, &$sub, &$com, $upfileInfo, $accessInfo){
+                if(!$this->check_captcha()){ die('You are not acting like a human!'); }
+        }
 }
